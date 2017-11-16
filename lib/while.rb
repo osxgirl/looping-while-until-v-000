7e3c7d1f4e582052cd8163d6687ddfdaaa20e587
Heather Marie Haas <osxgirl@mac.com>
2017-11-16 23:53:49 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,10 @@
 def using_while
-	using_while = 6
-	while using_while <= 10
-		using_while += 1
-		puts "Wingardium Leviosa"
-
-	end
+		using_while = 6
+		while using_while <= 10
+			using_while += 1
+			puts "Wingardium Leviosa"
+		end
+		
 	puts "Wingardium Leviosa"
 
 end
